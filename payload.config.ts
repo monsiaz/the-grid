16e22c1baadd59ea45d60d8e60dfe675f5550c75
Pierre-Louis <Pierrelouisdelcroix2001@gmail.com>
2026-03-30 14:57:51 -0400

    payload config fixed
    Signed-off-by: Pierre-Louis <Pierrelouisdelcroix2001@gmail.com>

--- payload.config.ts
+++ payload.config.ts
@@ -4,18 +4,18 @@ import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 import sharp from "sharp";
 
-import { Users } from "./collections/Users.ts";
-import { Media } from "./collections/Media.ts";
-import { Drivers } from "./collections/Drivers.ts";
-import { News } from "./collections/News.ts";
-import { TeamMembers } from "./collections/TeamMembers.ts";
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
+import { Drivers } from "./collections/Drivers";
+import { News } from "./collections/News";
+import { TeamMembers } from "./collections/TeamMembers";
 
-import { Homepage } from "./globals/Homepage.ts";
-import { AboutPage } from "./globals/AboutPage.ts";
-import { ServicesPage } from "./globals/ServicesPage.ts";
-import { ContactPage } from "./globals/ContactPage.ts";
-import { DriversPage } from "./globals/DriversPage.ts";
-import { SiteSettings } from "./globals/SiteSettings.ts";
+import { Homepage } from "./globals/Homepage";
+import { AboutPage } from "./globals/AboutPage";
+import { ServicesPage } from "./globals/ServicesPage";
+import { ContactPage } from "./globals/ContactPage";
+import { DriversPage } from "./globals/DriversPage";
+import { SiteSettings } from "./globals/SiteSettings";
 
 export default buildConfig({
   admin: {
